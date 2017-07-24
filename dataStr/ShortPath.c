7998df0d3eb318a142ae78c79863547af56d3a52
tianqichongzhen <tianqichongzhen@gmail.com>
2017-07-24 11:32:50 +0800

    2017-07-24

--- dataStr/ShortPath.c
+++ dataStr/ShortPath.c
@@ -1,4 +1,5 @@
 #include "include/MatrixGraph.h"
+#include "include/LStack.h"
 
 int D[VEX_NUM];  //从v0到终点vi的最短路径长度的数组
 Boolean P[VEX_NUM][VEX_NUM];  //从v0出发到终点vi的最短路径映射数组
@@ -47,7 +48,8 @@ void shortPath_DIJ(MGraph *G, int v0)
 			if(!final[w] && ((min + G->arcs[v][w].adj) < D[w]))
 			{
 				D[w] = min + G->arcs[v][w].adj;
-				for(int i = 0; i < G->vexnum; i++) P[w][i] = P[v][i];
+				//交换列
+				for(int i = 0; i < G->vexnum; i++) P[i][w] = P[i][v];
 				P[w][w] = TRUE;
 			}
 		}
@@ -55,10 +57,83 @@ void shortPath_DIJ(MGraph *G, int v0)
 	
 }
 
+Boolean singlePath(MGraph *G, int s, int d, int cost, Stack *S)
+{//源点s, 终点d, 当前已知的路径代价cost, 路径堆栈S
+	int n = pop(S);
+	for(int i = 0; i < G->vexnum; i++)
+	{
+		if((i == s) || (i == n)) continue;
+		if(P[n][i])
+		{
+			if(i == d)
+			{
+				cost = cost + G->arcs[n][i].adj;
+				if(cost == D[d])
+				{
+					push(S, n);
+					push(S, i);
+					return TRUE;
+				}else {
+					return FALSE;
+				}
+			}else {
+				cost = cost + G->arcs[n][i].adj;
+				push(S, n);
+				push(S, i);
+				if(singlePath(G, s, d, cost, S))
+				{
+					return TRUE;
+				}else {
+					pop(S);
+					cost = cost - G->arcs[n][i].adj;
+				}
+			}
+		}
+	}
+
+	return FALSE;	
+}
+
+Boolean getShortPath(MGraph *G, int s, int d)
+{//在图G中求从v点到d点的最短路径
+	shortPath_DIJ(G, s); //先求以v为源点的最短路径P[v]和带权长度D[v]
+	if(s == d)
+	{
+		printf("从%d点到%d点的最短路径为其本身\n");
+		return FALSE;
+	}
+
+	if(!P[d][d])
+	{
+		printf("%d点和%d点无路径相通\n", s, d);
+		return FALSE;
+	}
+
+	Stack *S = (Stack*)malloc(sizeof(Stack)); //保存最短路径堆栈
+	initStack(S);
+	for(int i = 0; i < G->vexnum; i++)
+	{
+		if(P[i][s])
+		{// 遍历第s列，s列表示与s有直接路径的顶点集
+			int cost = G->arcs[s][i].adj;
+			push(S, i);
+			singlePath(G, s, d, cost, S);
+		}
+	}
+
+	//求得最短路径
+	printf("从%d点到%d点的最短路径为(逆序)：", s, d);
+	while(!stackEmpty(S)) printf("%d ", pop(S));
+	printf("\n");
+
+	return TRUE;
+}
+
 Status main(void)
 {
 	MGraph *G = (MGraph*)malloc(sizeof(MGraph));
 	createGraph(G, DN);
+	getShortPath(G, 0, 5);
 
 	return OK;
 }
