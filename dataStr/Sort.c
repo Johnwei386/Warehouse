0ec7b39649c9cbd0faa8adf42cb5f214ff8aeea9
tianqichongzhen <tianqichongzhen@gmail.com>
2017-08-14 16:59:42 +0800

    2017-08-14

---
+++ dataStr/Sort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/Status.h"
+#define MAXSIZE 20
+
+typedef int KeyType;
+
+typedef struct{
+	KeyType key; //关键字项
+	int elem; //数据项
+}RedType; //记录类型
+
+typedef struct{
+	RedType R[MAXSIZE];
+	int length;
+}SqList; //顺序表类型
+
+void insertSort(SqList *L)
+{//对顺序表L作插入排序
+	int i,j,key;
+	int n = L->length;
+	for(i = 1; i < n; i++)
+	{
+		key = L->R[i].key;
+		for(j = i-1; (j >= 0) && (L->R[j].key > key); j--)
+			L->R[j+1].key = L->R[j].key;
+		L->R[j+1].key = key;
+	}
+}
+
+Status main(void)
+{
+	int a[] = {49, 38, 65, 97, 76, 13, 27, 50};
+	SqList *L = (SqList*)malloc(sizeof(SqList));
+	for(int i = 0; i < 8; i++)
+	{
+		L->R[i].key = a[i];
+		L->R[i].elem = 0;
+	}
+	L->length = 8;
+	insertSort(L);
+	printf("OK!\n");	
+
+	return OK;
+}
