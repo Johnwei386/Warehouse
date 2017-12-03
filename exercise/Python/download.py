8ee732d9b39a576fffb1c57e135f003ed85005e0
JohnWei <tianqichongzhen@gmail.com>
2017-12-03 21:50:22 +0800

    python download picture on file links

---
+++ exercise/Python/download.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import urllib,urllib2
+import re
+
+pattern = re.compile(r'(\d+)(\.).*')
+file = open('picture.link', 'r')
+url = file.readline()
+count = 0
+while url:
+	match = pattern.search(url)
+	if match:
+		filename = match.group()
+		urllib.urlretrieve(url,'./'+filename)
+		print filename+' ','has been downloaded'
+	url = file.readline()
+	count = count + 1
+print "总共下载了%d张图片" % count
+file.close()
