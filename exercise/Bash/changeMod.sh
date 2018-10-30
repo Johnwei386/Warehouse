aa73eb9a0e9a4727a4258d287ce7cbc563496e4d
tianqichongzhen <tianqichongzhen@gmail.com>
2018-10-30 16:24:41 +0800

    2018-10-30 changelog

---
+++ exercise/Bash/changeMod.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cpath=`pwd`
+changeMod(){
+    for file in $1/*
+    do
+         if [ -d $file ];then
+             cd $file
+             chmod 755 $file
+             changeMod $file
+             cd ..
+         else
+             chmod 644 $file
+         fi
+    done
+}
+
+changeMod $cpath
+
